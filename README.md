7baa467c07c57aea19db16800e712aaecc0b6540
jthyer <john.thyer@gmail.com>
2024-03-11 16:08:38 -0500

    Added extra files.

---
+++ README.md
@@ -0,0 +1,9 @@
+# template-project
+Basic template project for making games with Love2D and Ogmo.
+
+To Do:
+- I want easy simple animation and sprite switching. This should be handled at the parent object level. Maybe asset.sprite should be tables containing frame and image speed data?
+- Spikes. Player should be able to hit spikes and enemies and die.
+- Implement remaining jellyfish and bullets.
+- Very simple title screen, move between three stages, very simple ending. Do the title in-engine, like Game Maker.
+- Can still clip into corners. Do looping collision check to make sure player doesn't get stuck?
---
+++ source/class/player.lua
@@ -0,0 +1,117 @@
+local SPEED = 3
+local JUMP = 7
+local GRAVITY = 0.2
+local FASTFALL = 10
+local SLOWFALL = 5
+
+local function player(c,start_x,start_y,checkObjects)  
+  local public = {}
+  
+  local class, x, y = c, start_x, start_y
+  local sprite = global.getAsset("sprite","lillyR")
+  local hspeed, vspeed = 0, 0
+  local jumpRelease = false
+  local maxFallSpeed = FASTFALL
+  
+  local hitbox = {}
+  hitbox = {x=x+8,y=y+16,width=16,height=16}
+  
+  local function checkCollisionWalls(check_x,check_y,checkWidth,checkHeight,
+    floorOnly,ceilOnly)
+    local function f(obj) 
+      local collision = false
+      if obj.solid then
+        local x, y, w, h = obj.hitbox().x, obj.hitbox().y,
+          obj.hitbox().width, obj.hitbox().height
+        if (floorOnly == nil or check_y <= y) and
+          (ceilOnly == nil or check_y > y) and
+          util.checkOverlap(check_x,check_y,checkWidth,checkHeight,x,y,w,h) then
+          collision = true
+        end
+      end
+      
+      return collision 
+    end
+    return checkObjects(f)
+  end
+  
+  local function horMovement()
+    if kb.left() then
+      hspeed = -SPEED
+    elseif kb.right() then
+      hspeed = SPEED    
+    else
+      hspeed = 0
+    end
+
+    local collide_x = checkCollisionWalls(hitbox.x+hspeed,hitbox.y,
+      hitbox.width,hitbox.height)
+    if not collide_x then
+      x = x + hspeed
+    else
+      x = util.round(x/4) * 4
+    end  
+  end
+  
+  local function vertMovement()
+    local grounded = checkCollisionWalls(hitbox.x,hitbox.y+1,
+      hitbox.width,hitbox.height,true)
+  
+    if grounded then
+      if kb.jumpPressed() then
+        vspeed = -JUMP
+        jumpRelease = false
+      end
+    else  
+      maxFallSpeed = SLOWFALL
+      if (not kb.jumpHeld()) or
+        (jumpRelease and vspeed < 0) then
+        jumpRelease = true
+        if vspeed < -GRAVITY then
+          vspeed = vspeed + 0.5
+        end
+        maxFallSpeed = FASTFALL
+        vspeed = vspeed + (GRAVITY / 2)
+      end
+      vspeed = vspeed + GRAVITY
+      if vspeed > maxFallSpeed then
+        vspeed = maxFallSpeed
+      end
+    end  
+
+    local new_y = y + vspeed
+    local collide_y_floor = checkCollisionWalls(hitbox.x,hitbox.y+vspeed,
+      hitbox.width,hitbox.height,true)
+    local collide_y_ceil = checkCollisionWalls(hitbox.x,hitbox.y+vspeed,
+      hitbox.width,hitbox.height,nil,true)
+    
+    if not (collide_y_floor or collide_y_ceil) then
+      y = y + vspeed
+    else
+      y = util.round(y/16) * 16
+      if collide_y_floor then
+        vspeed = 0
+        vaccel = 0
+      end
+    end   
+  end
+  
+  local function updateHitbox()
+    hitbox.x = x+8
+    hitbox.y = y+16
+  end
+  
+  function public.update() 
+    horMovement()
+    vertMovement()
+    updateHitbox()
+  end
+  
+  function public.drawInfo() return x, y, sprite end
+  function public.get_y() return y end
+  function public.getSprite() return sprite end
+
+  return public
+end
+
+return player
